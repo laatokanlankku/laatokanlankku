b9380f042e8e11d4452f54936b71b6553c0a4c4e
matvei.kinner <matvei.kinner@cgi.com>
2019-10-06 22:31:34 +0300

    ll-002 [updt] Initial linting rules with eslint, prettier, airbnb, plugins, and husky

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
-const contentful = require("contentful");
+import React from 'react';
+
+const contentful = require('contentful');
 
 const App = () => {
+  /**
+   *
+   * Environment variable names must match those in the Netlify settings
+   * docs: https://www.netlify.com/docs/continuous-deployment/#environment-variables
+   *
+   */
   const CONTENTFUL_SPACE_ID = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
-  const CONTENTFUL_DELIVERY_TOKEN =
-    process.env.REACT_APP_CONTENTFUL_DELIVERY_TOKEN;
+  const CONTENTFUL_DELIVERY_TOKEN = process.env.REACT_APP_CONTENTFUL_DELIVERY_TOKEN;
 
   const client = contentful.createClient({
     space: CONTENTFUL_SPACE_ID,
-    accessToken: CONTENTFUL_DELIVERY_TOKEN
+    accessToken: CONTENTFUL_DELIVERY_TOKEN,
   });
 
   client
     .getEntries({
-      locale: "ru-RU"
+      locale: 'ru-RU',
     })
     .then(entry => console.log(entry))
     .catch(err => console.log(err));
